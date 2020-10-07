94ba2dab175ca1f97e8b562c71fe9ec0d58f65eb
ajpicard913 <Contact@saucedesigns.io>
2020-10-07 14:48:52 -0400

    Initial Commit

---
+++ Shared/Dribbble_WalletAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dribbble_WalletAppApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 10/7/20.
+//
+
+import SwiftUI
+
+@main
+struct Dribbble_WalletAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
